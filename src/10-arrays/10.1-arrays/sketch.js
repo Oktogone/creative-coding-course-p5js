994ddf5fd20f4bddd30c89d4cad328fc9a89a1b7
Aurelien Krieger <a.krieger.b@gmail.com>
2023-07-17 13:17:05 +0200

    variables and arrays

---
+++ src/10-arrays/10.1-arrays/sketch.js
@@ -0,0 +1,17 @@
+let players = ["Joana", "Pierrick", "Amélie", "Manu"];
+players[0];
+players[1];
+players[2];
+players[3];
+players[-1];
+players[4];
+players.length;
+players[players.length-1];
+
+let scores = [];
+scores.push(2);
+scores[0];
+scores.pop();
+
+players.splice(0, 3);
+players.splice(3);
---
+++ src/10-arrays/10.2-arrays-colours/sketch.js
@@ -0,0 +1,23 @@
+let colours;
+
+function setup() {
+  createCanvas(600, 600);
+  colours = [
+    color(214, 81, 49),
+    color(157, 83, 122),
+    color(237, 150, 58),
+    color(68, 87, 217),
+    color(237, 205, 69),
+    color(255, 255, 255),
+    color(0, 0, 0),
+    color(152, 152, 152),
+    color(106, 159, 58)
+  ];
+  frameRate(2);
+}
+
+function draw() {
+  let colorPicker = Math.floor(random(0, colours.length));
+  //background(colours[0]);
+  background(colours[colorPicker]);
+}
