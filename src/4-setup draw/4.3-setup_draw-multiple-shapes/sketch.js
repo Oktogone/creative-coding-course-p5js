ec7142a4dc1b6f4efbbfbb241408a8a3895cd9c5
Aurelien Krieger <a.krieger.b@gmail.com>
2023-08-21 19:48:04 +0200

    setup draw

--- src/4-setup draw/4.3-setup_draw-multiple-shapes/sketch.js
+++ src/4-setup draw/4.3-setup_draw-multiple-shapes/sketch.js
@@ -7,5 +7,6 @@ function draw() {
 
   //drawing two shapes on top of each other
   ellipse(mouseX, mouseY, 100, 100);
+  //ellipse(0, 0, 100, 100);
   ellipse(width * 0.5, height * 0.5, 100, 100);
 }
